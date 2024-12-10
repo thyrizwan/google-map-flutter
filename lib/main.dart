d76ad5fb1bf06fc4aa3a92d7e5df04a35ca0ef60
Rizwan Ansari <rizwangazi7466@gmail.com>
2024-12-10 12:56:56 +0530

    Second

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_geolocators/home_screen.dart';
 import 'package:google_maps_geolocators/map_screen.dart';
 
