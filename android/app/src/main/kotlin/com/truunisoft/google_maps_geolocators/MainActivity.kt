5f9b2c5645be44a08e9c73a788a39a18757c8c69
Rizwan Ansari <rizwangazi7466@gmail.com>
2024-12-10 10:59:21 +0530

    First

---
+++ android/app/src/main/kotlin/com/truunisoft/google_maps_geolocators/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.truunisoft.google_maps_geolocators
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Map Screen'),
+      ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(22.149888, 75.644989),
+        ),
+      ),
+    );
+  }
+}
